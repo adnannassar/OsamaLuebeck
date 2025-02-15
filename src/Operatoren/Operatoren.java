f371dbe4d11fc2466ddb6ef4b677949a4d30fdf8
Adnan Nassar <adnan.nassar90@gmail.com>
2025-02-15 14:36:06 +0100

    added: operatoren teil 1

---
+++ src/Operatoren/Operatoren.java
@@ -0,0 +1,38 @@
+package Operatoren;
+
+public class Operatoren {
+    public static void main(String[] args) {
+        // Operatoren in Java
+        // Arithmetische Operatoren (+, -, *, /, %)
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        int diff = a - b;
+        int prod = a * b;
+        int quot = a / b;
+        int rem = a % b;
+        System.out.println("Summe: " + sum);
+        System.out.println("Differenz: " + diff);
+        System.out.println("Produkt: " + prod);
+        System.out.println("Quotient: " + quot);
+        System.out.println("Rest: " + rem);
+
+
+        // Inkrement- und Dekrementoperatoren (++, --)
+        int x = 10; // x in speicher = 10
+        System.out.println(x++); // post inkrement, x ist 10 auf der konsole, x in speicher = 11
+        System.out.println(--x); // pre dekrement, x ist 10 auf der konsole, x in speicher = 10
+
+        // Zuweisungsoperatoren (=, +=, -=, *=, /=, %=)
+
+        // Vergleichsoperatoren (==, !=, <, >, <=, >=)
+
+        // Logische Operatoren (&&, ||, !)
+
+        // Bedingte Operatoren (?:)
+
+        // Instanzof-Operator
+
+
+    }
+}
